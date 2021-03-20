e5ff6656c00f8e5af7c03aa71e01e1a8bb6f6259
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-03-20 21:50:59 +0900

    api

---
+++ app/src/main/java/com/market/catchprice/Network/ApiClient.java
@@ -0,0 +1,33 @@
+package com.market.catchprice.Network;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://ec2-13-209-8-243.ap-northeast-2.compute.amazonaws.com/api/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+
+}
---
+++ app/src/main/java/com/market/catchprice/Network/ApiInterface.java
@@ -0,0 +1,10 @@
+package com.market.catchprice.Network;
+
+public class ApiInterface {
+
+   // @Multipart
+   // @POST("/api/auth/login")
+   // Call<LoginResponseInfo> Login(@Part("userID") RequestBody userID, @Part("userPassword") RequestBody userPassword); //로그인 데이터 검색
+
+
+}
