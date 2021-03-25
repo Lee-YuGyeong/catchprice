588e2bef804bb75e968432bdde10f0f56d0ef918
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-03-25 17:06:09 +0900

    image

--- app/src/main/java/com/market/catchprice/View/LoginActivity.java
+++ app/src/main/java/com/market/catchprice/View/LoginActivity.java
@@ -2,15 +2,28 @@ package com.market.catchprice.View;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.market.catchprice.Contract.LoginContract;
 import com.market.catchprice.Presenter.LoginPresenter;
 import com.market.catchprice.R;
@@ -20,7 +33,8 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
     private EditText id;
     private EditText pw;
     private Button btn_login;
-    private Button btn_signup;
+    Button btn_signup;
+    ImageView imageView,imageView2,imageView3,login_btn_img,signup_btn_img;
     private LoginContract.Presenter presenter;
 
     @Override
@@ -32,12 +46,42 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
         init();
     }
 
-    private void init() {
+    public void init() {
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        login_btn_img = (ImageView) findViewById(R.id.login_btn_img);
+        signup_btn_img = (ImageView) findViewById(R.id.signup_btn_img);
         id = (EditText) findViewById(R.id.id);
         pw = (EditText) findViewById(R.id.pw);
-
-
         btn_login = (Button) findViewById(R.id.login_btn);
+        btn_signup = (Button) findViewById(R.id.signup_btn);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+
+
+        Glide.with(this).load(R.drawable.text_login).into(imageView);
+        Glide.with(this).load(R.drawable.inputbox).into(imageView2);
+        Glide.with(this).load(R.drawable.inputbox).into(imageView3);
+        Glide.with(this).load(R.drawable.checkbox).into(login_btn_img);
+        Glide.with(this).load(R.drawable.text_signup).into(signup_btn_img);
+        Glide.with(this)
+                .asBitmap()
+                .load(R.drawable.background_login)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        Drawable drawable = new BitmapDrawable(getResources(), resource);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            layout.setBackground(drawable);
+                        }
+                    }
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) { }
+                });
+
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,7 +89,6 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
             }
         });
 
-        btn_signup = (Button) findViewById(R.id.signup_btn);
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
